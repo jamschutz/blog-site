2d7bf3e6e38bd7731b547ad3eb68fe7a92df60d8
Joey Schutz <schutzjoey@gmail.com>
2023-11-18 14:22:27 -0500

    switching to static site generator

---
+++ .eleventy.js
@@ -0,0 +1,28 @@
+const PostCSSPlugin = require("eleventy-plugin-postcss")
+
+module.exports = function(eleventyConfig) {
+    // -- constants --
+    const srcDir = "src"
+    const dstDir = "_site"
+    
+
+    // -- assets --
+    eleventyConfig.addPlugin(PostCSSPlugin);
+    eleventyConfig.addPassthroughCopy('img');
+    eleventyConfig.addPassthroughCopy('fonts');
+
+    
+    // -- build --
+    // remove the _collections dir from the site output
+    eleventyConfig.on("eleventy.after", async () => {
+        await rm(`${dstDir}/_collections`, { recursive: true, force: true })
+    })
+
+
+    return {
+        dir: {
+            input: srcDir,
+            output: dstDir
+        }
+    };
+}
---
+++ postcss.config.js
@@ -0,0 +1,10 @@
+const config = {
+    plugins: [
+      require("postcss-import"),
+      require("postcss-simple-vars"),
+      require("autoprefixer"),
+    ],
+  }
+  
+  module.exports = config
+  
